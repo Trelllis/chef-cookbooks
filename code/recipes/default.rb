fb4202921eb5d34f6e88b68a65ed5c0ffd42fa83
Abed Halawi <halawi.abed@gmail.com>
2015-09-14 00:31:43 +0300

    add logs

--- code/recipes/default.rb
+++ code/recipes/default.rb
@@ -1,19 +1,17 @@
 include_recipe 'deploy'
 
 node[:deploy].each do |application, deploy|
-  if deploy[:application_type] != 'static'
-    Chef::Log.debug("Skipping deploy::web application #{application} as it is not an static HTML app")
-    next
-  end
+    Chef::Log.debug(">>>> Deploying application #{application}")
 
-  opsworks_deploy_dir do
-    user deploy[:user]
-    group deploy[:group]
-    path deploy[:deploy_to]
-  end
+    opsworks_deploy_dir do
+        user deploy[:user]
+        group deploy[:group]
+        path deploy[:deploy_to]
+    end
 
-  opsworks_deploy do
-    app application
-    deploy_data deploy
-  end
+    opsworks_deploy do
+        app application
+        deploy_data deploy
+    end
+    Chef::Log.debug(">>>> Deployment of application #{application} complete.")
 end
