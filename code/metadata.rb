5f91d4015844c1f4d721aa20f6c44648a14812a7
Abed Halawi <halawi.abed@gmail.com>
2015-09-15 12:23:37 +0300

    update metadata
    - specify composer cookbook dependency
    - add provided recipes

--- code/metadata.rb
+++ code/metadata.rb
@@ -5,3 +5,9 @@ license     "MIT"
 version     "1.0.0"
 
 depends "deploy"
+depends "composer"
+
+recipe "code::deploy", "Deploy a version of the code"
+recipe "code::mount", "A workaround for Docker running a symlink directory"
+recipe "code::install_dependencies", "Run composer install inside the project directory"
+recipe "code::symlink_storage", "Move the storage directory into a shared directory so that it can be used by each release"
