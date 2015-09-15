143166819111f4a04b907d2ddde5cc574381c2c4
Abed Halawi <halawi.abed@gmail.com>
2015-09-15 11:59:35 +0300

    create mount directory before mounting

--- code/recipes/mount.rb
+++ code/recipes/mount.rb
@@ -1,8 +1,16 @@
 node[:deploy].each do |application, deploy|
+
+  directory "/home/ec2-user/#{application}" do
+    owner deploy[:user]
+    group deploy[:group]
+    action :create
+  end
+
   script "mount" do
     interpreter "bash"
     code <<-EOH
     sudo mount -o bind #{deploy[:deploy_to]}/current /home/ec2-user/#{application}
     EOH
   end
+
 end
