c8ca0bee7566b84c80e8cd7712534f513f164caa
Abed Halawi <halawi.abed@gmail.com>
2015-09-15 17:26:44 +0300

    add code::env recipe and template

---
+++ code/recipes/env.rb
@@ -0,0 +1,14 @@
+node[:deploy].each do |application, deploy|
+  template "#{deploy[:deploy_to]}/current/.env" do
+    source ".env.erb"
+    mode 0755
+    owner deploy[:user]
+    group deploy[:group]
+
+    variables(
+      variables: (deploy[:environment_variables] rescue {}),
+    )
+
+    only_if { ::File.directory?("#{deploy[:deploy_to]}/current") }
+  end
+end
