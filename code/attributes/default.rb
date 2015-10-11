5ace3fc8bd0edae037d8b6f77cba1ea35d7c56cf
Abed Halawi <halawi.abed@gmail.com>
2015-10-11 18:12:18 +0300

    fix code deployment location

--- code/attributes/default.rb
+++ code/attributes/default.rb
@@ -1,5 +1,5 @@
 node[:deploy].each do |application, deploy|
     normal[:deploy][application][:user] = 'ec2-user'
     normal[:deploy][application][:group] = 'ec2-user'
-    normal[:deploy][application][:deploy_to] = '/home/ec2-user/code'
+    normal[:deploy][application][:deploy_to] = "/home/ec2-user/code/#{application}"
 end
