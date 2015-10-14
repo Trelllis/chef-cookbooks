91aad1d842b7a0b2919a238770f18226eb8c6ebd
Abed Halawi <halawi.abed@gmail.com>
2015-10-14 13:48:46 +0300

    fix the issue where an already installed php package conflicts with compose

--- code/attributes/default.rb
+++ code/attributes/default.rb
@@ -1,5 +1,8 @@
+node['composer']['php_recipe'] = 'code::noop'
+
 node[:deploy].each do |application, deploy|
     normal[:deploy][application][:user] = 'ec2-user'
     normal[:deploy][application][:group] = 'ec2-user'
     normal[:deploy][application][:deploy_to] = "/home/ec2-user/code/#{application}"
 end
+
