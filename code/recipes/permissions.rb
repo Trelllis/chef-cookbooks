a06512e03063ae5d23a3a6065b728e520abfe61b
Abed Halawi <halawi.abed@gmail.com>
2015-10-15 04:06:23 +0300

    change bootstrap directory's mode in permissions

--- code/recipes/permissions.rb
+++ code/recipes/permissions.rb
@@ -1,8 +1,17 @@
 node[:deploy].each do |application, deploy|
-  script "open_storage_directory" do
-    interpreter "bash"
-    user 'root'
-    cwd deploy[:deploy_to]
-    code "chmod -R o+w storage"
-  end
+
+    script "open_storage_directory" do
+        interpreter "bash"
+        user 'root'
+        cwd deploy[:deploy_to]
+        code "chmod -R o+w storage"
+    end
+
+    script "open_bootstrap_directory" do
+        interpreter "bash"
+        user 'root'
+        cwd deploy[:deploy_to]
+        code "chmod -R o+w bootstrap"
+    end
+
 end
