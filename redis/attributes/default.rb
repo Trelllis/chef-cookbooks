e7e02346940ba881aa453db584f2f07cfdb12465
Abed Halawi <halawi.abed@gmail.com>
2015-11-26 01:57:25 +0200

    syntax at me again...

--- redis/attributes/default.rb
+++ redis/attributes/default.rb
@@ -1,3 +1,5 @@
+node[:redis] = []
+node[:redis][:stores] = []
 node[:redis][:stores][:data] = {title: 'data', port: 6379}
 node[:redis][:stores][:media] = {title: 'media', port: 6389}
 node[:redis][:stores][:cache] = {title: 'cache', port: 6399}
