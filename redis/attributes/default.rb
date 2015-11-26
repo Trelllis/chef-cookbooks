e7502dcdd6290e5971a4c662c8c1b1f038d0d5bd
Abed Halawi <halawi.abed@gmail.com>
2015-11-26 11:05:26 +0200

    add the private ip of the primary cluster

--- redis/attributes/default.rb
+++ redis/attributes/default.rb
@@ -1,6 +1,6 @@
 default[:redis] = {}
 default[:redis][:stores] = []
-default[:redis][:stores].push({title: 'data', port: '6379', ip: '52.30.8.129'})
-default[:redis][:stores].push({title: 'media', port: '6389', ip: '52.30.8.129'})
-default[:redis][:stores].push({title: 'cache', port: '6399', ip: '52.30.8.129'})
+default[:redis][:stores].push({title: 'data', port: '6379', ip: '10.0.3.60'})
+default[:redis][:stores].push({title: 'media', port: '6389', ip: '10.0.3.60'})
+default[:redis][:stores].push({title: 'cache', port: '6399', ip: '10.0.3.60'})
 
