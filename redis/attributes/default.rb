6f6d013a891a07f837bdadf0567313c2d7a519d6
Abed Halawi <halawi.abed@gmail.com>
2015-11-26 02:03:11 +0200

    use default instead of node

--- redis/attributes/default.rb
+++ redis/attributes/default.rb
@@ -1,6 +1,6 @@
-node[:redis] = []
-node[:redis][:stores] = []
-node[:redis][:stores][:data] = {title: 'data', port: 6379}
-node[:redis][:stores][:media] = {title: 'media', port: 6389}
-node[:redis][:stores][:cache] = {title: 'cache', port: 6399}
+default[:redis] = []
+default[:redis][:stores] = []
+default[:redis][:stores][:data] = {title: 'data', port: 6379}
+default[:redis][:stores][:media] = {title: 'media', port: 6389}
+default[:redis][:stores][:cache] = {title: 'cache', port: 6399}
 
