e6986f3aa3899738f9e8ae0bd39cc2eee3ad8287
Abed Halawi <halawi.abed@gmail.com>
2015-11-26 04:35:47 +0200

    only download docker-compose when it doesn't exist

--- redis/recipes/configure.rb
+++ redis/recipes/configure.rb
@@ -11,6 +11,8 @@ script "install docker-compose" do
         curl -L https://github.com/docker/compose/releases/download/1.5.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
         chmod +x /usr/local/bin/docker-compose
     EOH
+
+    not_if do ::File.exists?('/usr/local/bin/docker-compose') end
 end
 
 sysctl_param 'vm.swappiness' do
