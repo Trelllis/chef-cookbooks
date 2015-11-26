c67141644406cb11ec1d818cae997ec61f523494
Abed Halawi <halawi.abed@gmail.com>
2015-11-26 02:10:34 +0200

    more syntax changes

--- redis/attributes/default.rb
+++ redis/attributes/default.rb
@@ -1,6 +1,6 @@
-default[:redis] = []
+default[:redis] = {}
 default[:redis][:stores] = []
-default[:redis][:stores][:data] = {title: 'data', port: 6379}
-default[:redis][:stores][:media] = {title: 'media', port: 6389}
-default[:redis][:stores][:cache] = {title: 'cache', port: 6399}
+default[:redis][:stores].push({title: 'data', port: 6379})
+default[:redis][:stores].push({title: 'media', port: 6389})
+default[:redis][:stores].push({title: 'cache', port: 6399})
 
