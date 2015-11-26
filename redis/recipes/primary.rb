5aea015ff5d418fdd40cf6ccca156974609db7b9
Abed Halawi <halawi.abed@gmail.com>
2015-11-26 02:36:12 +0200

    fix syntax issue

--- redis/recipes/primary.rb
+++ redis/recipes/primary.rb
@@ -4,7 +4,7 @@ node['redis']['stores'].each do |store|
     # create data directories
 
     # run primary stores
-    docker-compose "redis-#{store[:title]}-primary" do
+    docker-compose 'redis-primary-instance' do
         action :up
         source 'redis-primary-compose.yml.erb'
     end
