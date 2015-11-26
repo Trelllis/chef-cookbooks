59c33bec992a8a4b8f1339d0630bc9a0dbdf510a
Abed Halawi <halawi.abed@gmail.com>
2015-11-26 02:26:27 +0200

    fix typo in recipe name

--- redis/recipes/primary.rb
+++ redis/recipes/primary.rb
@@ -4,7 +4,7 @@ node['redis']['stores'].each do |store|
     # create data directories
 
     # run primary stores
-    docker_compose "redis-#{store[:title]}-primary" do
+    docker-compose "redis-#{store[:title]}-primary" do
         action :up
         source 'redis-primary-compose.yml.erb'
     end
