57e3b6b161c7e367cd73443605a87d1e00b9fb5a
Abed Halawi <halawi.abed@gmail.com>
2015-11-27 11:09:18 +0200

    go inside the backend to update trellis containers

--- trellis/recipes/update.rb
+++ trellis/recipes/update.rb
@@ -1,11 +1,11 @@
-template '/home/ec2-user/code/docker-compose.yml' do
+template '/home/ec2-user/code/trellis_backend/docker-compose.yml' do
     source 'trellis-compose.yml.erb'
 end
 
 # run docker compose
 script 'trellis' do
     interpreter 'bash'
-    cwd "/home/ec2-user/code"
+    cwd "/home/ec2-user/code/trellis_backend"
     code <<-EOH
         docker-compose pull
         docker-compose stop
