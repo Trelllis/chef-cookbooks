11ffdd2e065f58de5c5a4c34b6745ace8e93d290
Abed Halawi <halawi.abed@gmail.com>
2015-12-09 13:58:28 +0200

    [trellis] add dev recipe to run nginx for the dev env

--- trellis/metadata.rb
+++ trellis/metadata.rb
@@ -4,5 +4,6 @@ maintainer "Vinelab"
 license "MIT"
 version "1.0.0"
 
+recipe "trellis::dev", "Run Trellis for the Development environment"
 recipe "trellis::run", "Run Trellis"
 recipe "trellis::update", "Update the running containers (pull, stop, up)"
---
+++ trellis/recipes/dev.rb
@@ -0,0 +1,12 @@
+template '/home/ec2-user/code/docker-compose.yml' do
+    source 'trellis-dev-compose.yml.erb'
+end
+
+# run docker compose
+script 'trellis' do
+    interpreter 'bash'
+    cwd "/home/ec2-user/code"
+    code <<-EOH
+        docker-compose up -d --no-recreate
+    EOH
+end
