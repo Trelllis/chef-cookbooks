3c09991458d4e643451808f447a6e5c6e1d0e9fe
Abed Halawi <halawi.abed@gmail.com>
2015-12-21 02:23:10 +0200

    [neo4j] use the actual home dir instead of the user's home dir

--- neo4j/recipes/default.rb
+++ neo4j/recipes/default.rb
@@ -1,14 +1,14 @@
-directory "#{ENV['HOME']}/neo4j"
-directory "#{ENV['HOME']}/neo4j/data"
+directory "/home/ec2-user/neo4j"
+directory "/home/ec2-user/neo4j/data"
 
-template "#{ENV['HOME']}/neo4j/docker-compose.yml" do
+template "/home/ec2-user/neo4j/docker-compose.yml" do
     source 'neo4j-compose.yml.erb'
 end
 
 # run docker compose
 script 'neo4j' do
     interpreter 'bash'
-    cwd "#{ENV['HOME']}/neo4j"
+    cwd "/home/ec2-user/neo4j"
     code <<-EOH
         docker-compose up -d --no-recreate
     EOH
