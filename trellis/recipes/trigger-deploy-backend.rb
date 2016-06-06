a111c003245980f866fa7a8680603d6a45b3e5c0
Jad Chamoun <jad.chamoun@gmail.com>
2016-06-06 18:33:09 +0300

    Update backend recipe

--- trellis/recipes/trigger-deploy-backend.rb
+++ trellis/recipes/trigger-deploy-backend.rb
@@ -1,5 +1,5 @@
-instance_opsworks_id = node["opsworks"]["instance"]["id"]
-stack_id = node["opsworks"]["stack"]["id"]
+instance_opsworks_id = node[:opsworks:][:instance:][:id:]
+stack_id = node[:opsworks][:stack][:id:]
 app_id = "77357f70-5f0f-4737-9df0-1d51857f5d98"
 
     
