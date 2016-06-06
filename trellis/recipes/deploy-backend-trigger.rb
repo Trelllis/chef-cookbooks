bdbd77db51987fa287f127480f3ae166df1328c3
Jad Chamoun <jad.chamoun@gmail.com>
2016-06-06 17:35:33 +0300

    Add Trellis frontend and backend deploy app trigger

---
+++ trellis/recipes/deploy-backend-trigger.rb
@@ -0,0 +1,11 @@
+instance-opsworks-id = node["opsworks"]["instance"]["id"]
+stack-id = node["opsworks"]["stack"]["id"]
+app-id = "77357f70-5f0f-4737-9df0-1d51857f5d98"
+
+    
+    bash 'Deploy-App' do
+        code <<-EOH
+            aws opsworks --region us-east-1 create-deployment --instance-ids "#{instance-opsworks-id}" --stack-id #{stack-id} --app-id #{app-id} --command "{\"Name\":\"deploy\"}"
+        EOH
+
+    end
---
+++ trellis/recipes/deploy-frontend-trigger.rb
@@ -0,0 +1,11 @@
+instance-opsworks-id = node["opsworks"]["instance"]["id"]
+stack-id = node["opsworks"]["stack"]["id"]
+app-id = "8aac1be5-0d09-40f9-90e3-10fdcfba1a43"
+
+    
+    bash 'Deploy-App' do
+        code <<-EOH
+            aws opsworks --region us-east-1 create-deployment --instance-ids "#{instance-opsworks-id}" --stack-id #{stack-id} --app-id #{app-id} --command "{\"Name\":\"deploy\"}"
+        EOH
+
+    end
