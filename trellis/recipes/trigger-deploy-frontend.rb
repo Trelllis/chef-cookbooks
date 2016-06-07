b02ea6b069157a45b3ae98529552d4cf80f09945
Jad Chamoun <jad.chamoun@gmail.com>
2016-06-07 14:18:50 +0300

    remove square brackets

--- trellis/recipes/trigger-deploy-frontend.rb
+++ trellis/recipes/trigger-deploy-frontend.rb
@@ -5,7 +5,7 @@ Chef::Log.info("aws opsworks --region us-east-1 create-deployment --instance-ids
 script 'Deploy-App' do
     interpreter 'bash'
     code <<-EOH
-aws opsworks --region us-east-1 create-deployment --instance-ids \"[#{node[:instance]['id']}]\" --stack-id #{node[:instance]['stack_id']} --app-id #{node[:instance]['frontend_app_id']} --command #{command}
+aws opsworks --region us-east-1 create-deployment --instance-ids \"#{node[:instance]['id']}\" --stack-id #{node[:instance]['stack_id']} --app-id #{node[:instance]['frontend_app_id']} --command #{command}
 EOH
 
 end
