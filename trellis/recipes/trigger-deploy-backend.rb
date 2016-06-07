7476319c051891ffec4fb5c0cf37d6ed39e17476
Jad Chamoun <jad.chamoun@gmail.com>
2016-06-07 10:31:41 +0300

    Trellis backend trigger recipe test

--- trellis/recipes/trigger-deploy-backend.rb
+++ trellis/recipes/trigger-deploy-backend.rb
@@ -1,11 +1,7 @@
-instance_opsworks_id: node[:opsworks][:instance][:id]
-stack_id: node[:opsworks][:stack][:id]
-app_id: "77357f70-5f0f-4737-9df0-1d51857f5d98"
-
-    
-    bash 'Deploy-App' do
+ bash 'Deploy-App' do
         code <<-EOH
-            aws opsworks --region us-east-1 create-deployment --instance-ids "#{instance-opsworks-id}" --stack-id #{stack-id} --app-id #{app-id} --command "{\"Name\":\"deploy\"}"
+            aws opsworks --region us-east-1 create-deployment --instance-ids "#{node[:opsworks][:instance][:id]}" --stack-id #{node[:opsworks][:stack][:id]} --app-id 77357f70-5f0f-4737-9df0-1d51857f5d98 --command "{\"Name\":\"deploy\"}"
         EOH
 
     end
+
