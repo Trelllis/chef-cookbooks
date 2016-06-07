8e214e7b5c0bb92bd5316a717b52ac36f8bd81d6
Abed Halawi <halawi.abed@gmail.com>
2016-06-07 11:27:18 +0300

    extract attributes to the attributes/default.rb file

--- trellis/attributes/default.rb
+++ trellis/attributes/default.rb
@@ -7,3 +7,8 @@ default[:hosts]['redis']['redis-primary-cache'] = ''
 default[:hosts]['redis']['redis-replica-media'] = ''
 default[:hosts]['redis']['redis-replica-data'] = ''
 default[:hosts]['redis']['redis-replica-cache'] = ''
+
+default[:instance] = {}
+default[:instance]['id'] = node["opsworks"]["instance"]["id"]
+default[:instance]['stack_id'] = node["opsworks"]["stack"]["id"]
+default[:instance]['backend_app_id'] = '8aac1be5-0d09-40f9-90e3-10fdcfba1a43'
--- trellis/recipes/trigger-deploy-frontend.rb
+++ trellis/recipes/trigger-deploy-frontend.rb
@@ -1,11 +1,6 @@
-instance_opsworks_id = node["opsworks"]["instance"]["id"]
-stack_id = node["opsworks"]["stack"]["id"]
-app_id = "8aac1be5-0d09-40f9-90e3-10fdcfba1a43"
+bash 'Deploy-App' do
+    code <<-EOH
+        aws opsworks --region us-east-1 create-deployment --instance-ids "#{node[:instance]['id']}" --stack-id #{node[:instance]['stack_id']} --app-id #{node[:instance]['backend_app_id']} --command "{\"Name\":\"deploy\"}"
+    EOH
 
-    
-    bash 'Deploy-App' do
-        code <<-EOH
-            aws opsworks --region us-east-1 create-deployment --instance-ids "#{instance-opsworks-id}" --stack-id #{stack-id} --app-id #{app-id} --command "{\"Name\":\"deploy\"}"
-        EOH
-
-    end
+end
