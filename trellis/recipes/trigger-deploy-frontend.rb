8fa6b79f25dfba8aa59c8096cc09b21c27e77250
Jad Chamoun <jad.chamoun@gmail.com>
2016-06-07 12:38:59 +0300

    Edit command to deploy

--- trellis/recipes/trigger-deploy-frontend.rb
+++ trellis/recipes/trigger-deploy-frontend.rb
@@ -1,9 +1,9 @@
 #Chef::Log.info("app-id [#{node[:instance]['backend_app_id']}]")
-Chef::Log.info("stack-id [#{node[:instance]['stack_id']}]")
+#Chef::Log.info("stack-id [#{node[:instance]['stack_id']}]")
 #Chef::Log.info("app-id [#{node[:instance]['id']}]")
-#bash 'Deploy-App' do
-#    code <<-EOH
-#        aws opsworks --region us-east-1 create-deployment --instance-ids "#{node[:instance]['id']}" --stack-id #{node[:instance]['stack_id']} --app-id #{node[:instance]['backend_app_id']}
-#EOH
+bash 'Deploy-App' do
+    code <<-EOH
+        aws opsworks --region us-east-1 create-deployment --instance-ids %Q[#{node[:instance]['stack_id']}]%Q --stack-id [#{node[:instance]['stack_id']}] --app-id [#{node[:instance]['stack_id']}]
+EOH
 
 #end
