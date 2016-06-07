f26e44458885706e7b2525605ac295038394f3fb
Abed Halawi <halawi.abed@gmail.com>
2016-06-07 13:58:06 +0300

    log command

--- trellis/recipes/trigger-deploy-frontend.rb
+++ trellis/recipes/trigger-deploy-frontend.rb
@@ -1,6 +1,6 @@
 command = '"{\"Name\":\"deploy\"}"'
 
-Chef.log("aws opsworks --region us-east-1 create-deployment --instance-ids \"[#{node[:instance]['id']}]\" --stack-id [#{node[:instance]['stack_id']}] --app-id [#{node[:instance]['frontend_app_id']}] --command [#{command}]")
+Chef::log("aws opsworks --region us-east-1 create-deployment --instance-ids \"[#{node[:instance]['id']}]\" --stack-id [#{node[:instance]['stack_id']}] --app-id [#{node[:instance]['frontend_app_id']}] --command [#{command}]")
 
 script 'Deploy-App' do
     interpreter 'bash'
