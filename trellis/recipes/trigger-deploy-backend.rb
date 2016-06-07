2f1baa19a3b690b9c44389c753f258aa1da909c5
Jad Chamoun <jad.chamoun@gmail.com>
2016-06-07 10:38:11 +0300

    Fix JSON Error in trellis backend trigger

--- trellis/recipes/trigger-deploy-backend.rb
+++ trellis/recipes/trigger-deploy-backend.rb
@@ -1,6 +1,6 @@
  bash 'Deploy-App' do
         code <<-EOH
-            aws opsworks --region us-east-1 create-deployment --instance-ids "#{node[:opsworks][:instance][:id]}" --stack-id #{node[:opsworks][:stack][:id]} --app-id 77357f70-5f0f-4737-9df0-1d51857f5d98 --command "{\"Name\":\"deploy\"}"
+            aws opsworks --region us-east-1 create-deployment --instance-ids "#{node[:opsworks][:instance][:id]}" --stack-id #{node[:opsworks][:stack][:id]} --app-id 77357f70-5f0f-4737-9df0-1d51857f5d98 --command \"{\"Name\":\"deploy\"}\"
         EOH
 
     end
