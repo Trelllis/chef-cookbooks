15c79b9dbc9aed95600b3c7233df37bbeb5c7f68
Abed Halawi <halawi.abed@gmail.com>
2016-06-07 14:14:09 +0300

    remove square brackets

--- trellis/recipes/trigger-deploy-frontend.rb
+++ trellis/recipes/trigger-deploy-frontend.rb
@@ -1,11 +1,11 @@
 command = '"{\"Name\":\"deploy\"}"'
 
-Chef::Log.info("aws opsworks --region us-east-1 create-deployment --instance-ids \"[#{node[:instance]['id']}]\" --stack-id [#{node[:instance]['stack_id']}] --app-id [#{node[:instance]['frontend_app_id']}] --command [#{command}]")
+Chef::Log.info("aws opsworks --region us-east-1 create-deployment --instance-ids \"[#{node[:instance]['id']}]\" --stack-id #{node[:instance]['stack_id']} --app-id #{node[:instance]['frontend_app_id']} --command #{command}")
 
 script 'Deploy-App' do
     interpreter 'bash'
     code <<-EOH
-aws opsworks --region us-east-1 create-deployment --instance-ids \"[#{node[:instance]['id']}]\" --stack-id [#{node[:instance]['stack_id']}] --app-id [#{node[:instance]['frontend_app_id']}] --command [#{command}]
+aws opsworks --region us-east-1 create-deployment --instance-ids \"[#{node[:instance]['id']}]\" --stack-id #{node[:instance]['stack_id']} --app-id #{node[:instance]['frontend_app_id']} --command #{command}
 EOH
 
 end
