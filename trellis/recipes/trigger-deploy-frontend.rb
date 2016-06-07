d104b81adcf0f95ba30cc8606982e44bd68ed642
Jad Chamoun <jad.chamoun@gmail.com>
2016-06-07 12:23:25 +0300

    update chef log

--- trellis/recipes/trigger-deploy-frontend.rb
+++ trellis/recipes/trigger-deploy-frontend.rb
@@ -1,7 +1,8 @@
 log 'message' do
-    message 'app-id #{node[:instance]["backend_app_id"]}'
+    message 'app-id [#{node[:instance]['backend_app_id']}]'
     level :info
 end
+Chef::Log.info("app-id [#{node[:instance]['backend_app_id']}]")
 #bash 'Deploy-App' do
 #    code <<-EOH
 #        aws opsworks --region us-east-1 create-deployment --instance-ids "#{node[:instance]['id']}" --stack-id #{node[:instance]['stack_id']} --app-id #{node[:instance]['backend_app_id']}
