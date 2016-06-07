2b1bbf5833be6e5e5756346fa1cf81e9248dd80e
Jad Chamoun <jad.chamoun@gmail.com>
2016-06-07 12:50:03 +0300

    Add command and trellis frontend app id

--- trellis/attributes/default.rb
+++ trellis/attributes/default.rb
@@ -11,4 +11,5 @@ default[:hosts]['redis']['redis-replica-cache'] = ''
 default[:instance] = {}
 default[:instance]['id'] = node["opsworks"]["instance"]["id"]
 default[:instance]['stack_id'] = node["opsworks"]["stack"]["id"]
-default[:instance]['backend_app_id'] = '8aac1be5-0d09-40f9-90e3-10fdcfba1a43'
+default[:instance]['backend_app_id'] = '77357f70-5f0f-4737-9df0-1d51857f5d98'
+default[:instance]['frontend_app_id'] = '8aac1be5-0d09-40f9-90e3-10fdcfba1a43'
