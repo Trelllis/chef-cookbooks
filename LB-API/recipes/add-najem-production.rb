740a82520df950b27f8c0e6b19968c757e4d2591
Jad Chamoun <jad.chamoun@gmail.com>
2016-06-14 12:50:55 +0300

    Fixed http_request params error

--- LB-API/recipes/add-najem-production.rb
+++ LB-API/recipes/add-najem-production.rb
@@ -1,6 +1,6 @@
 #API Request to add instance to Load Balancer
 
-http_request do
+http_request '' do
     
     url "http://#{node[:haproxy]['production_ip']}:8001/api/add/?instance_id=#{node[:instance]['id']}&backend=najem&port_numb=80&private_ip=#{node[:instance]['private_ip']}"
 
--- LB-API/recipes/remove-instance-stage.rb
+++ LB-API/recipes/remove-instance-stage.rb
@@ -1,6 +1,6 @@
 #API Request to remove instance from Load Balancer
 
-http_request do
+http_request '' do
     
     url "http://#{node[:haproxy]['stage_ip']}:8001/api/remove/?instance_id=#{node[:instance]['id']}&port_numb=80&private_ip=#{node[:instance]['private_ip']}"
 
