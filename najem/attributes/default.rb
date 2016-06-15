33dcd0b17b0d44f208dac0af7e12d9d08b8e6158
Jad Chamoun <jad.chamoun@gmail.com>
2016-06-15 18:59:35 +0300

    Add attributes to najem default

--- najem/attributes/default.rb
+++ najem/attributes/default.rb
@@ -1,4 +1,6 @@
 default[:instance] = {}
 default[:instance]['id'] = node["opsworks"]["instance"]["id"]
 default[:instance]['stack_id'] = node["opsworks"]["stack"]["id"]
+default[:instance]['backend_app_id'] = '77357f70-5f0f-4737-9df0-1d51857f5d98'
+default[:instance]['frontend_app_id'] = '8aac1be5-0d09-40f9-90e3-10fdcfba1a43'
 default[:instance]['najem_frontend_app_id'] = '27aa639a-226a-4afa-93f5-1e76936ec12f'
