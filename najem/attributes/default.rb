5199801e07615a45dfca639704136ccd713cfb0a
Jad Chamoun <jad.chamoun@gmail.com>
2016-06-15 18:36:08 +0300

    Fix najem conflict in deplyment frontend

--- najem/attributes/default.rb
+++ najem/attributes/default.rb
@@ -1,4 +1,4 @@
 default[:instance] = {}
 default[:instance]['id'] = node["opsworks"]["instance"]["id"]
 default[:instance]['stack_id'] = node["opsworks"]["stack"]["id"]
-default[:instance]['frontend_app_id'] = '27aa639a-226a-4afa-93f5-1e76936ec12f'
+default[:instance]['najem_frontend_app_id'] = '27aa639a-226a-4afa-93f5-1e76936ec12f'
--- najem/recipes/trigger-deploy-frontend.rb
+++ najem/recipes/trigger-deploy-frontend.rb
@@ -5,7 +5,7 @@ command = '"{\"Name\":\"deploy\"}"'
 script 'Deploy-App' do
     interpreter 'bash'
     code <<-EOH
-aws opsworks --region us-east-1 create-deployment --instance-ids \"#{node[:instance]['id']}\" --stack-id #{node[:instance]['stack_id']} --app-id #{node[:instance]['frontend_app_id']} --command #{command}
+aws opsworks --region us-east-1 create-deployment --instance-ids \"#{node[:instance]['id']}\" --stack-id #{node[:instance]['stack_id']} --app-id #{node[:instance]['najem_frontend_app_id']} --command #{command}
 EOH
 
 end
