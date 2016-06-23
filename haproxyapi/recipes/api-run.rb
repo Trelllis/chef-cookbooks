20fd80619fc8ae58e1a37a8132bfbfd2b76bcb67
Jad Chamoun <jad.chamoun@gmail.com>
2016-06-23 13:26:24 +0300

    Include requirements.txt for pip

--- haproxyapi/recipes/api-run.rb
+++ haproxyapi/recipes/api-run.rb
@@ -1,44 +1,3 @@
-script 'install_pip' do
-    interpreter 'bash'
-    code <<-EOH
-    sudo pip install django
-EOH
-
-end
-
-script 'install_django' do
-    interpreter 'bash'
-    code <<-EOH
-    sudo pip install django
-EOH
-
-end
-
-script 'install_djangorestframework' do
-    interpreter 'bash'
-    code <<-EOH
-sudo pip install djangorestframework
-EOH
-
-end
-
-
-script 'install_slackcommunicator' do
-    interpreter 'bash'
-    code <<-EOH
-    sudo pip install slackweb
-EOH
-
-end
-
-script 'install_uwsgi' do
-    interpreter 'bash'
-    code <<-EOH
-    sudo pip install uwsgi
-EOH
-
-end
-
 script 'clone_code' do
     interpreter 'bash'
     cwd "/"
@@ -48,10 +7,11 @@ EOH
 
 end
 
-script 'prepare_log_directory' do
+script 'install_requirements' do
     interpreter 'bash'
+    cwd "/haproxy-config-manager"
     code <<-EOH
-    sudo mkdir /var/log/uwsgi/
+    sudo pip install -r requirements.txt
 EOH
 
 end
@@ -60,7 +20,7 @@ script 'launch_deamon' do
     interpreter 'bash'
     cwd "/haproxy-config-manager"
     code <<-EOH
-    sudo uwsgi --http :8001 --wsgi-file ProxyAPI/wsgi.py --enable-threads --daemonize=/var/log/uwsgi/api.log
+    sudo uwsgi --http :8001 --wsgi-file ProxyAPI/wsgi.py --enable-threads --daemonize=/haproxy-config-manager/uwsgi-api.log
 EOH
 
 end
