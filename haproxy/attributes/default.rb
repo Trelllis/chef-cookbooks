58acffe2b9c2475ba694afb74bc6bc601096d8cf
Jad Chamoun <jad.chamoun@gmail.com>
2016-06-27 12:07:36 +0300

    Remove haproxy init to avoid conflicts

--- haproxy/attributes/default.rb
+++ haproxy/attributes/default.rb
@@ -1,9 +1,6 @@
 default[:instance] = {}
 default[:instance]['instance_id'] = node['opsworks']['instance']['aws_instance_id']
 default[:instance]['private_ip'] = node['opsworks']['instance']['private_ip']
-default[:haproxy] = {}
-#default[:haproxy]['stage_ip'] = '10.0.7.211'
-#default[:haproxy]['production_ip'] = '192.168.9.53'
 default[:haproxy]['private_ip'] = node['haproxy_ip']
 default[:haproxy]['backend'] = node['backend']
 default[:instance]['id'] = node["opsworks"]["instance"]["id"]
