47558b04df86b133bc3151b7a9bd4f514fb0e924
Jad Chamoun <jad.chamoun@gmail.com>
2016-06-27 15:31:49 +0300

    Add comments and attributes to default haproxy

--- haproxy/attributes/default.rb
+++ haproxy/attributes/default.rb
@@ -1,6 +1,9 @@
+#General Attributes
 default[:instance] = {}
 default[:instance]['instance_id'] = node['opsworks']['instance']['aws_instance_id']
 default[:instance]['private_ip'] = node['opsworks']['instance']['private_ip']
+
+#haproxy server specific attributes
 default[:haproxy][:check_interval] = '10s'
 default[:haproxy][:client_timeout] = '60s'
 default[:haproxy][:server_timeout] = '60s'
@@ -9,9 +12,17 @@ default[:haproxy][:connect_timeout] = '10s'
 default[:haproxy][:http_request_timeout] = '30s'
 default[:haproxy][:global_max_connections] = '80000'
 default[:haproxy][:default_max_connections] = '80000'
+default[:haproxy][:retries] = '3'
+default[:haproxy][:httpclose] = true
+default[:haproxy][:http_server_close] = false
+default[:haproxy][:stats_socket_path] = '/tmp/haproxy.sock'
+
+#haproxy api specific attributes
 default[:haproxyapi] = {}
 default[:haproxyapi]['private_ip'] = node['haproxy_ip']
 default[:haproxyapi]['backend'] = node['backend']
+
+#haproxy opsworks related attributes
 default[:instance]['id'] = node["opsworks"]["instance"]["id"]
 default[:instance]['stack_id'] = node["opsworks"]["stack"]["id"]
 default[:instance]['backend_app_id_stage'] = '77357f70-5f0f-4737-9df0-1d51857f5d98'
