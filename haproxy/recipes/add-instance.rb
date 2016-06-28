0cfedd14b182217e67c8fd869185d3196433f3b6
Jad Chamoun <jad.chamoun@gmail.com>
2016-06-28 11:41:24 +0300

    Fix error in HAProxy API Call

--- haproxy/recipes/add-instance.rb
+++ haproxy/recipes/add-instance.rb
@@ -2,6 +2,6 @@
 
 http_request '' do
     
-    url "http://#{node[:haproxyapi]['private_ip']}:8001/api/add/?instance_id=#{node[:instance]['instance_id']}&backend=#{node[:haproxyapi]['backend']}&port_numb=80&private_ip=#{node[:instance]['private_ip']}&type-#{node["opsworks"]["instance"]["instance_type"]}"
+    url "http://#{node[:haproxyapi]['private_ip']}:8001/api/add/?instance_id=#{node[:instance]['instance_id']}&backend=#{node[:haproxyapi]['backend']}&port_numb=80&private_ip=#{node[:instance]['private_ip']}&type=#{node["opsworks"]["instance"]["instance_type"]}"
 
 end
