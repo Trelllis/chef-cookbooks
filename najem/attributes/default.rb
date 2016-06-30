8bd67dad47cf188016d1a7ff0fcecfe05020b7fb
Jad Chamoun <jad.chamoun@gmail.com>
2016-06-30 14:43:13 +0300

    Change variables backend and frontend app id to distinguish between najem and stage

--- najem/attributes/default.rb
+++ najem/attributes/default.rb
@@ -1,4 +1,6 @@
 default[:instance] = {}
 default[:instance]['id'] = node["opsworks"]["instance"]["id"]
 default[:instance]['stack_id'] = node["opsworks"]["stack"]["id"]
-default[:instance]['frontend_app_id'] =node["frontend_app_id"]
+default[:instance]['trellis_backend_app_id'] = node["trellis_backend_app_id"]
+default[:instance]['najem_frontend_app_id'] =node["najem_frontend_app_id"]
+default[:instance]['trellis_frontend_app_id'] =node["trellis_frontend_app_id"]
--- trellis/recipes/trigger-deploy-backend.rb
+++ trellis/recipes/trigger-deploy-backend.rb
@@ -5,7 +5,7 @@ command = '"{\"Name\":\"deploy\"}"'
 script 'Deploy-App' do
     interpreter 'bash'
     code <<-EOH
-aws opsworks --region us-east-1 create-deployment --instance-ids \"#{node[:instance]['id']}\" --stack-id #{node[:instance]['stack_id']} --app-id #{node[:instance]['backend_app_id']} --command #{command}
+aws opsworks --region us-east-1 create-deployment --instance-ids \"#{node[:instance]['id']}\" --stack-id #{node[:instance]['stack_id']} --app-id #{node[:instance]['trellis_backend_app_id']} --command #{command}
 EOH
 
 end
