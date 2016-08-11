51bc92b744e40f88d9a511cea73b47e7e53b6c2e
Jad Chamoun <jad.chamoun@gmail.com>
2016-08-11 15:51:14 +0300

    Fix variable problem

--- code/recipes/deploy-s3.rb
+++ code/recipes/deploy-s3.rb
@@ -2,34 +2,34 @@ script 'create_dir' do
     interpreter 'bash'
     cwd "/home"
     code <<-EOH
-    mkdir -p /home/ec2-user/code/{node[:deploy]['layer']}_frontend/public
+    mkdir -p /home/ec2-user/code/#{node[:deploy]['layer']}_frontend/public
 EOH
 
 end
 
 script 'clone_code' do
     interpreter 'bash'
-    cwd "/home/ec2-user/code/{node[:deploy]['layer']}_frontend/public"
+    cwd "/home/ec2-user/code/#{node[:deploy]['layer']}_frontend/public"
     code <<-EOH
-aws s3 cp s3://vinelab-code/{node[:deploy]['layer']}_frontend/{node[:deploy]['env']}.zip {node[:deploy]['env']}.zip
+aws s3 cp s3://vinelab-code/#{node[:deploy]['layer']}_frontend/#{node[:deploy]['env']}.zip #{node[:deploy]['env']}.zip
 EOH
 
 end
 
 script 'unzip_code' do
     interpreter 'bash'
-    cwd "/home/ec2-user/code/{node[:deploy]['layer']}_frontend/public"
+    cwd "/home/ec2-user/code/#{node[:deploy]['layer']}_frontend/public"
     code <<-EOH
-    sudo unzip {node[:deploy]['env']}.zip
+    sudo unzip #{node[:deploy]['env']}.zip
 EOH
 
 end
 
 script 'remove_archive' do
     interpreter 'bash'
-    cwd "/home/ec2-user/code/{node[:deploy]['layer']}_frontend/public"
+    cwd "/home/ec2-user/code/#{node[:deploy]['layer']}_frontend/public"
     code <<-EOH
-    sudo rm {node[:deploy]['env']}.zip
+    sudo rm #{node[:deploy]['env']}.zip
 EOH
 
 end
