1283d310116e8b83b06e6b2489caa4e8e6bd021d
Jad Chamoun <jad.chamoun@gmail.com>
2016-08-11 15:31:52 +0300

    Update deployment script

--- code/attributes/default.rb
+++ code/attributes/default.rb
@@ -3,3 +3,5 @@ node[:deploy].each do |application, deploy|
     normal[:deploy][application][:group] = 'ec2-user'
     normal[:deploy][application][:deploy_to] = "/home/ec2-user/code/#{application}"
 end
+default[:deploy]['env']=node['env']
+default[:deploy]['layer']=node['backend']
