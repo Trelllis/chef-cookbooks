d7a89b35b847ec7a60db7aaa1232eb2baca6b8ec
Jad Chamoun <jad.chamoun@gmail.com>
2016-08-16 15:20:17 +0300

    Merge deploy s3 and git

--- code/recipes/deploy.rb
+++ code/recipes/deploy.rb
@@ -9,78 +9,68 @@ node[:deploy].each do |application, deploy|
         action :create
     end
 
-    # Create id_rsa file - Git SSH Key
-    file "/tmp/id_rsa" do
-        owner deploy[:user]
-        mode "0400"
-        content deploy[:scm][:ssh_key]
-    end
+    if deploy["scm"]["scm_type"] == 'git'
+        
+        # Create id_rsa file - Git SSH Key
+        file "/tmp/id_rsa" do
+            owner deploy[:user]
+            mode "0400"
+            content deploy[:scm][:ssh_key]
+        end
 
-    # Create SSH wrapper script
-    file "/tmp/git_wrapper.sh" do
-        owner deploy[:user]
-        mode "0755"
-        content "#!/bin/bash\n/usr/bin/env ssh -q -2 -o \"StrictHostKeyChecking=no\" -i \"/tmp/id_rsa\" $1 $2"
-    end
-    
-#    script 'create_dir' do
-#        interpreter 'bash'
-#        cwd [:deploy_to]
-#        code <<-EOH
-#        mkdir -p public
-#EOH
-#
-#    end
-#    
-#    script 'clone_code' do
-#        interpreter 'bash'
-#        cwd [:deploy_to]
-#        code <<-EOH
-#    aws s3 cp s3://vinelab-code/najem_frontend/stage.zip .
-#EOH
-#    end
-#
-#    script 'unzip_code' do
-#        interpreter 'bash'
-#        cwd [:deploy_to]
-#        code <<-EOH
-#        sudo unzip *.zip
-#EOH
-#
-#    end
-#
-#    script 'remove_archive' do
-#        interpreter 'bash'
-#        cwd [:deploy_to]
-#        code <<-EOH
-#        sudo rm *.zip
-#EOH
-#
-#    end
+        # Create SSH wrapper script
+        file "/tmp/git_wrapper.sh" do
+            owner deploy[:user]
+            mode "0755"
+            content "#!/bin/bash\n/usr/bin/env ssh -q -2 -o \"StrictHostKeyChecking=no\" -i \"/tmp/id_rsa\" $1 $2"
+        end
 
-    s3_bucket, s3_key, base_url = OpsWorks::SCM::S3.parse_uri(deploy[:scm][:repository])
-    
-    s3_file "#{deploy[:deploy_to]}/stage.zip" do
-#        remote_path "/najem_frontend/stage.zip"
-#        bucket "vinelab-code"
-        bucket s3_bucket
-        remote_path s3_key
-        aws_access_key_id deploy["scm"]["user"]
-        aws_secret_access_key deploy["scm"]["password"]
-        s3_url base_url
-        owner deploy[:user]
-        group deploy[:group]
-        action :create
+        # Deploy code
+        git "deploy #{application}" do
+            destination deploy[:deploy_to]
+            repository deploy[:scm][:repository]
+            user deploy[:user]
+            group deploy[:group]
+            revision deploy[:scm][:revision]
+            ssh_wrapper "/tmp/git_wrapper.sh"
+            action :sync
+        end
+        
     end
     
-#    # Deploy code
-#    git "deploy #{application}" do
-#        destination deploy[:deploy_to]
-#        repository deploy[:scm][:repository]
-#        user deploy[:user]
-#        group deploy[:group]
-#        revision deploy[:scm][:revision]
-#        ssh_wrapper "/tmp/git_wrapper.sh"
-#        action :sync
-#    end
+    if deploy["scm"]["scm_type"] == 's3'
+
+        s3_bucket, s3_key, base_url = OpsWorks::SCM::S3.parse_uri(deploy[:scm][:repository])
+
+        s3_file "#{deploy[:deploy_to]}/code.zip" do
+            bucket s3_bucket
+            remote_path s3_key
+            aws_access_key_id deploy["scm"]["user"]
+            aws_secret_access_key deploy["scm"]["password"]
+            s3_url base_url
+            owner deploy[:user]
+            group deploy[:group]
+            action :create
+        end
+
+        script 'unzip_code' do
+            interpreter 'bash'
+            cwd [:deploy_to]
+            code <<-EOH
+            sudo unzip code.zip
+EOH
+
+        end
+
+        script 'remove_archive' do
+            interpreter 'bash'
+            cwd [:deploy_to]
+            code <<-EOH
+            sudo rm code.zip
+EOH
+
+        end
+        
+    end
+    
 end
